62a6ad50672971f1a83dadfb445204bafbd102f4
DannyHatcher <danny@dannyhatcher.com>
2021-02-28 15:25:19 +0000

    Variable

---
+++ Basics/Variable.py
@@ -0,0 +1,8 @@
+name = "George"  # String
+age = 70  # Number
+print("There once was a man named " + name + ",")
+print("he was " + str(age) + " years old.")
+
+name = "Danny"
+print("He really liked the name " + name + " ,")
+print("but didn't like being " + str(age) + ".")

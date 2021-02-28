eda2b31a9d7f01614def0e7094d9dcbc8a6a19fd
DannyHatcher <danny@dannyhatcher.com>
2021-02-28 15:45:47 +0000

    String

---
+++ Basics/Strings.py
@@ -0,0 +1,13 @@
+print("Giraffe\nAcademy")  # \n = new line
+Name = "Danny Hatcher"
+
+print(Name.lower())  # Making it all lower
+print(Name.capitalize())  # Capetalize first word
+print(Name.upper())  # Making it all upper
+print(Name.islower())  # Checking if it is all lower
+print(Name.isupper())  # checking if it is all upper
+print(Name.upper().isupper())  # Make it upper and check it is upper
+print(len(Name))  # Count character length
+print(Name[0])  # Prints the number in the string (starting with 0)
+print(Name.index("D"))  # Gives the first location in the string
+print(Name.replace("Danny", "Jade"))  # Replacing characters with others

d11cb06c8ffe93df8251b54f3fd04cfa67d06960
DannyHatcher <danny@dannyhatcher.com>
2021-03-04 14:36:14 +0000

    commit

--- Basics/Input-calculator.py
+++ Basics/Input-calculator.py
@@ -1,14 +1,14 @@
 print("Write first number symbol second number and seporate with space")
-incalc = input("")  # Asking for the calcuation
+input_calculation = input("")  # Asking for the calcuation
 
-outcalc = incalc.split(" ")
-# splitting the incalc stuff (using space " " ) and making the result outcalc
+output_calcluation = input_calculation.split(" ")
+# splitting the input_calculation stuff (using space " " ) and making the result output_calcluation
 
-num1 = float(outcalc[0])
-symbol = outcalc[1]
-num2 = float(outcalc[2])
+num1 = float(output_calcluation[0])
+symbol = output_calcluation[1]
+num2 = float(output_calcluation[2])
 
-# could replace if with elif in the latter part
+# Calculating the input
 if symbol == "-":
     print(num1 - num2)
 elif symbol == "+":
@@ -17,3 +17,5 @@ elif symbol == "/":
     print(num1/num2)
 elif symbol == "*":
     print(num1*num2)
+else:
+    print("Invalid Symbol")

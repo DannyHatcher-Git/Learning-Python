50f2900e56012dd817178bf153d259adc6932f80
DannyHatcher <danny@dannyhatcher.com>
2021-03-10 22:47:16 +0000

    while loop

---
+++ Basics/while-loop.py
@@ -0,0 +1,5 @@
+i = 1
+while i <= 10:  # This starts a loop
+    print(i)  # Print this if the avoce is true
+    i += 1  # Add 1 to i
+print("Done with loop")  # Always printed after the loop finishes
